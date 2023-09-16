6d6ebf46570312da5092dd336c1f8dba684d6ebb
Bijendra Raut <61984063+BijendraRaut@users.noreply.github.com>
2023-09-16 16:57:40 +0530

    feat - fetch data and rendered the names

---
+++ Employee Database Management/src/index.js
@@ -0,0 +1,46 @@
+(async function () {
+  const data = await fetch("./src/data.json");
+  const json = await data.json();
+  let employees = json;
+  let selectedEmployeeid = employees.id;
+  let selectedEmployee = employees;
+
+  const employeeList = document.querySelector(".employees__names--list");
+  const employeeInfo = document.querySelector(".employee__single--info");
+
+  employeeList.addEventListener("click", (e) => {
+    if (e.target.tagName === "SPAN" && selectedEmployeeid !== e.target.id) {
+      selectedEmployeeid = e.target.id;
+      renderEmployees();
+      renderSingleEmployee();
+    }
+  });
+
+  const renderEmployees = () => {
+    employeeList.innerHTML = "";
+
+    employees.forEach((emp) => {
+      const employee = document.createElement("span");
+      employee.classList.add("employees__names--item");
+
+      if (parseInt(selectedEmployeeid, 10) === emp.id) {
+        employee.classList.add("selected");
+        selectedEmployee = emp;
+      }
+
+      employee.setAttribute("id", emp.id);
+      employee.innerHTML = `${emp.firstName} ${emp.lastName} <i class="employeeDelete">❌</i>`;
+
+      employeeList.append(employee);
+    });
+  };
+
+  const renderSingleEmployee = () => {};
+  renderEmployees();
+})();
+
+/*
+* feat - fetch data and rendered the names
+* style - dashboard styled
+
+*/
